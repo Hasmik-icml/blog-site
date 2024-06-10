8015cc4f65bcb94f5ed06b2fc7ad33ddadbeb3b0
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-06-10 17:40:34 +0400

    Change frontend styles for test

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,24 +1,24 @@
 // For testing
 
-// const body = document.getElementById("test");
+const body = document.getElementById("test");
 
-// async function fetchWords() {
-//     fetch(`http://localhost:3010/menu-submenu/${'menu'}`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-//       return response.json();
-//     })
-//     .then(data => {
-//       console.log(111, data);
-//       body.innerHTML = data[0];
-//     })
-//     .catch(error => {
-//       console.error('There was a problem with the fetch operation:', error);
-//     });
+async function fetchWords() {
+    fetch(`http://localhost:3010/menu-submenu/${'menu'}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(111, data);
+      body.innerHTML =`${data.data[0].name}`;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 
-// }
-// document.addEventListener('DOMContentLoaded', () => {
-//     fetchWords();
-//   });
+}
+document.addEventListener('DOMContentLoaded', () => {
+    fetchWords();
+  });
