85b01bb5052d097fd727f3e10035f7be8bdf229b
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-08-10 00:07:18 +0400

    Change not-found handler

--- backend/src/handlers/not-found.handler.ts
+++ backend/src/handlers/not-found.handler.ts
@@ -1,14 +1,22 @@
+import { ValidationError } from "express-validator";
 import { CustomError } from "../errors/custom.error";
+import { BadRequestError } from "./bad-request.handler";
 
 export class NotFoundError extends CustomError {
   statusCode = 404;
 
-  constructor(public message: string) {
-    super(message);
+  constructor(public errors: ValidationError[] | string) {
+    super(typeof errors === 'string' ? errors : 'Invalid request parameters');
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 
   serializeErrors() {
-    return [{ message: this.message }];
+    if (typeof this.errors === 'string') {
+      return [{ message: this.errors }];
+    } else {
+      return [{
+        message: this.errors[0].msg,
+      }];
+    }
   }
 }
