5464af3749d4f3e75a9e7f1c121e23609a6fa1af
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-08-10 00:34:04 +0400

    Improve error messages for menu/subMenu

--- backend/src/routers/menu.routers.ts
+++ backend/src/routers/menu.routers.ts
@@ -1,15 +1,27 @@
 import { Router } from "express";
 import { body, param } from "express-validator";
 import { MenuSubMenuController } from "../controller/menuSubMenu.controller";
+import { validateRequest } from "../middleware/validation.middleware";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const
     router: Router = Router();
 router
     .post("/create/:infotype",
         [
-            param("infotype").isString().isIn(["menu", "subMenu"]).withMessage("Invalid infotype"),
-            body('name').trim().escape().isString().notEmpty().withMessage("Please provide a name for the menu/submenu"),
-            body('url').trim().isString().notEmpty().isURL().withMessage("URL should be a string"),
+            param("infotype")
+                .isString()
+                .isIn(["menu", "subMenu"]).withMessage("Infotype nust be 'menu', 'subMenu'"),
+            body('name')
+                .trim()
+                .escape()
+                .isString().withMessage("The name of menu/submenu should be a string")
+                .notEmpty().withMessage("Please provide a name for the menu/submenu"),
+            body('url')
+                .trim()
+                .isString().withMessage("URL should be a string")
+                .notEmpty()
+                .isURL().withMessage("Please provide a valid URL"),
             body('order').optional().isInt().withMessage("Order should be an integer"),
             body('show').optional().isBoolean().withMessage("Show should be a boolean"),
             body('menuId')
@@ -19,27 +31,35 @@ router
                 .isInt()
                 .withMessage("Menu ID should be an integer"),
         ],
+        validateRequest,
+        authMiddleware,
         MenuSubMenuController.create,
     )
     .get("/:infotype",
         [
-            param("infotype").isString().isIn(["menu", "subMenu"]).withMessage("Invalid infotype"),
+            param("infotype").isString().isIn(["menu", "subMenu"]).withMessage("Infotype nust be 'menu', 'subMenu'"),
         ],
+        validateRequest,
         MenuSubMenuController.findAllMenusAndSubMenus
     )
     .patch("/:infotype/:id",
         [
-            param("infotype").isString().isIn(["menu", "subMenu"]).withMessage("Invalid infotype"),
-            body('name').optional().trim().escape().isString().withMessage("Please provide a name for the menu/submenu"),
-            body('url').optional().trim().isString().isURL().withMessage("URL should be a string"),
+            param("infotype").isString().isIn(["menu", "subMenu"]).withMessage("Infotype nust be 'menu', 'subMenu'"),
+            body('name')
+                .optional()
+                .trim()
+                .escape()
+                .isString().withMessage("The name of menu/submenu should be a string"),
+            body('url').optional().trim().isString().withMessage("URL should be a string").isURL().withMessage("Please provide a valid URL"),
             body('order').optional().isInt().withMessage("Order should be an integer"),
             body('show').optional().isBoolean().withMessage("Show should be a boolean"),
             body('menuId')
                 .if((value, { req }) => req.params?.infotype === "subMenu")
                 .optional()
-                .isInt()
-                .withMessage("Menu ID should be an integer"),
+                .isInt().withMessage("Menu ID should be an integer"),
         ],
+        validateRequest,
+        authMiddleware,
         MenuSubMenuController.updateMenuSubMenu
     )
     .delete("/:infotype/:id",
@@ -47,6 +67,8 @@ router
             param("infotype").isString().isIn(["menu", "subMenu"]).withMessage("Invalid infotype"),
             param('id').isInt().withMessage('ID must be an integer'),
         ],
+        validateRequest,
+        authMiddleware,
         MenuSubMenuController.deleteMenuSubMenu
     )
 
--- backend/src/services/menuSubMenu.service.ts
+++ backend/src/services/menuSubMenu.service.ts
@@ -1,10 +1,8 @@
-import { Menu, PrismaClient, SubMenu } from "@prisma/client";
+import { Menu, Prisma, PrismaClient, SubMenu } from "@prisma/client";
+import { NotFoundError } from "../handlers/not-found.handler";
 
 export class MenuSubMenuService {
     public static async create(name: string, url: string, order: number, show: boolean, menuId: number, type: string): Promise<Menu | SubMenu> {
-        if (type !== 'menu' && type !== 'subMenu') {
-            throw new Error('Invalid type provided');
-        }
         const client = new PrismaClient();
 
         try {
@@ -39,49 +37,41 @@ export class MenuSubMenuService {
     }
 
     public static async FindAllMenusAndSubmenus(type: string): Promise<[data: Menu[] | SubMenu[], count: number]> {
-        if (type !== 'menu' && type !== 'subMenu') {
-            throw new Error('Invalid type provided');
-        }
         const client = new PrismaClient();
 
-        try {
-            const menuRepo = client.menu;
-            const subMneuRepo = client.subMenu;
-            let result: [data: Menu[] | SubMenu[], count: number] | null = null;
+        const menuRepo = client.menu;
+        const subMneuRepo = client.subMenu;
+        let result: [data: Menu[] | SubMenu[], count: number] | null = null;
 
-            if (type === "menu") {
-                const [data, count] = await client.$transaction([
-                    menuRepo.findMany({
-                        where: { deletedAt: null, },
-                        include: { subMenus: true }
-                    }),
-                    menuRepo.count({
-                        where: { deletedAt: null, }
-                    })
-                ]);
-                result = [data, count];
-            } else if (type === "subMenu") {
-                const [data, count] = await client.$transaction([
-                    subMneuRepo.findMany({
-                        where: { deletedAt: null, },
-                        include: { menu: true }
-                    }),
-                    subMneuRepo.count({
-                        where: { deletedAt: null, }
-                    })
-                ]);
-                result = [data, count];
-            }
-
-            if (!result) {
-                throw new Error('Creation failed');
-            }
-
-            return result;
-        } catch (error) {
-            console.log(555, error);
-            throw new Error("Error creating menu/submenu: ");
+        if (type === "menu") {
+            const [data, count] = await client.$transaction([
+                menuRepo.findMany({
+                    where: { deletedAt: null, },
+                    include: { subMenus: true }
+                }),
+                menuRepo.count({
+                    where: { deletedAt: null, }
+                })
+            ]);
+            result = [data, count];
+        } else if (type === "subMenu") {
+            const [data, count] = await client.$transaction([
+                subMneuRepo.findMany({
+                    where: { deletedAt: null, },
+                    include: { menu: true }
+                }),
+                subMneuRepo.count({
+                    where: { deletedAt: null, }
+                })
+            ]);
+            result = [data, count];
         }
+
+        if (!result) {
+            throw new NotFoundError('Error fetching menu/subMenu');
+        }
+
+        return result;
     }
 
     public static async updateMenuSubMenu(id: number, type: string, data: Partial<Menu | SubMenu>) {
@@ -114,53 +104,46 @@ export class MenuSubMenuService {
 
             return result;
         } catch (error) {
-            console.error('Error updating menu/submenu:', error);
+            if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+                throw new NotFoundError('SubMenu not found');
+            }
             throw new Error('Error updating menu/submenu');
         }
     }
 
     public static async deleteMenuSubMenu(id: number, type: string) {
-        if (type !== 'menu' && type !== 'subMenu') {
-            throw new Error('Invalid type provided');
-        }
-        try {
-            const client = new PrismaClient();
-            const menuRepo = client.menu;
-            const subMenuRepo = client.subMenu;
+        const client = new PrismaClient();
+        const menuRepo = client.menu;
+        const subMenuRepo = client.subMenu;
 
-            let result;
-            let deleted;
+        let result;
+        let deleted;
 
-            if (type === 'menu') {
-                result = await menuRepo.findUnique({
+        if (type === 'menu') {
+            result = await menuRepo.findUnique({
+                where: { id },
+            });
+            if (result) {
+                deleted = await menuRepo.update({
                     where: { id },
+                    data: { deletedAt: new Date() },
                 });
-                if (result) {
-                    deleted = await menuRepo.update({
-                        where: { id },
-                        data: { deletedAt: new Date() },
-                    });
-                }
-            } else if (type === 'subMenu') {
-                result = await subMenuRepo.findUnique({
+            }
+        } else if (type === 'subMenu') {
+            result = await subMenuRepo.findUnique({
+                where: { id },
+            });
+            if (result) {
+                deleted = await subMenuRepo.update({
                     where: { id },
+                    data: { deletedAt: new Date() },
                 });
-                if (result) {
-                    deleted = await subMenuRepo.update({
-                        where: { id },
-                        data: { deletedAt: new Date() },
-                    });
-                }
             }
-
-            if (!result) {
-                return null;
-            }
-
-            return deleted;
-        } catch (error) {
-            console.error('Error deleting menu/submenu:', error);
-            throw new Error('Error deleting menu/submenu');
         }
+
+        if (!result) {
+            throw new NotFoundError('SubMenu not found');
+        }
+        return deleted;
     }
 }
